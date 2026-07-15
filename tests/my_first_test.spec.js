a319dc02d7649d0f2767906bcf999698e25675f6
Arpithpoojary001 <arpithks244@gmail.com>
2026-07-15 13:48:38 +0530

    Delete tests directory

--- tests/my_first_test.spec.js
+++
@@ -1,6 +0,0 @@
-const {test, expect} =require('@playwright/test')
-
-test('my first test', async ({page})=>{
-    await page.goto('https://playwright.dev/')
-    await expect(page).toHaveTitle('Fast and reliable end-to-end testing for modern web apps | Playwright')
-})
